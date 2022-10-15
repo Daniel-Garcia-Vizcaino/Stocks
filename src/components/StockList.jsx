7e736ca253932c1d928289896fc5e8c7fc41b6d8
DanielGV <35636612+Daniel-Garcia-Vizcaino@users.noreply.github.com>
2022-10-15 18:46:32 +0000

    Set up axios

--- src/components/StockList.jsx
+++ src/components/StockList.jsx
@@ -1,3 +1,6 @@
+import { useState, useEffect } from 'react'
 export const StockList = () => {
+  const [watchList, setWatchList] = useState(["GOOGL", "MSFT", "AMZ"])
+  
   return <div>StockList</div>
 } 
