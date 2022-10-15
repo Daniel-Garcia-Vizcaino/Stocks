2d1692b9a99b71e57e2c8eb46e6bb082a30e9f04
DanielGV <35636612+Daniel-Garcia-Vizcaino@users.noreply.github.com>
2022-10-15 18:06:08 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import './App.css'
+
+export default function App() {
+  return (
+    <main>
+      <h2  className="mt-5">Hello</h2>
+    </main>
+  )
+}
