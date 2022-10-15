1116525b65e999442f8bfebed7bb9f17dea8953c
DanielGV <35636612+Daniel-Garcia-Vizcaino@users.noreply.github.com>
2022-10-15 18:56:05 +0000

    Set up FetchData

--- src/components/StockList.jsx
+++ src/components/StockList.jsx
@@ -1,6 +1,21 @@
 import { useState, useEffect } from 'react'
+import finnHub from '../apis/finnHub'
+
 export const StockList = () => {
   const [watchList, setWatchList] = useState(["GOOGL", "MSFT", "AMZ"])
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try{
+        const response = await finnHub.get("/quote?symbol=MSFT&token=cd5f85aad3i5nc8nu3a0cd5f85aad3i5nc8nu3ag")
+        console.log(response)
+      }catch(err){
+        
+      }
+    }
+    fetchData()
+  }, [])
+
   
   return <div>StockList</div>
 } 
