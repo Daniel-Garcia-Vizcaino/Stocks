8be29251d76fbfb2851682090485733cdd2b1dbf
DanielGV <35636612+Daniel-Garcia-Vizcaino@users.noreply.github.com>
2022-10-16 20:39:52 +0000

    Set up table headers

--- src/components/StockList.jsx
+++ src/components/StockList.jsx
@@ -41,5 +41,20 @@ export const StockList = () => {
   }, [])
 
   
-  return <div>StockList</div>
+  return <div>
+    <table className="table hover mt-5">
+      <thead style={{color: "rgb(79,89,102)"}}>
+        <tr>
+          <th scope="col">Name</th>
+          <th scope="col">Last</th>
+          <th scope="col">Chg</th>
+          <th scope="col">Chg%</th>
+          <th scope="col">High</th>
+          <th scope="col">Low</th>
+          <th scope="col">Open</th>
+          <th scope="col">Pclose</th>
+        </tr>
+      </thead>
+    </table>
+  </div>
 } 
