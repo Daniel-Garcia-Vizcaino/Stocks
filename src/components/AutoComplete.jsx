9c45bc6dec7bb3a81b7e520301fb6242d40f16b8
DanielGV <35636612+Daniel-Garcia-Vizcaino@users.noreply.github.com>
2022-10-20 14:33:03 +0000

    Set up fetchData for the AutoComplete component

--- src/components/AutoComplete.jsx
+++ src/components/AutoComplete.jsx
@@ -1,8 +1,44 @@
+import { useState, useEffect } from "react"
+import finnHub from "../apis/finnHub"
+
+
 export const AutoComplete = () => {
+  const [search, setSearch] = useState("")
+  const [results, setResults] = useState([])
+
+  useEffect(() => {
+    let isMounted = true
+    const fetchData = async () => {
+      try{
+        const response = await finnHub.get("/search", {
+          params: {
+            q: search
+          }
+        })
+        console.log(response)
+        if(isMounted){
+          setResults(response.data.result)
+        }
+        
+      } catch (err) {
+        
+      }
+    }
+    if (search.length > 0){
+     fetchData() 
+    }
+    return () => (isMounted = false)
+  }, [search])
+  
   return <div className="w-50 rounded mx-auto">
     <div className="form-floating dropdown">
-      <input style={{backgroundColor: "rgba(146, 157, 172, 0.04)"}} id="search" type="text" className="form-control" placeholder="Search" autoComplete="off"></input>
+      <input style={{backgroundColor: "rgba(146, 157, 172, 0.04)"}} id="search" type="text" className="form-control" placeholder="Search" autoComplete="off" value={search} onChange={(e) => setSearch(e.target.value)}></input>
       <label htmlFor="search">Search</label>
+      <ul className="dropdown-menu">
+        <li>stock1</li>
+        <li>stock2</li>
+        <li>stock3</li>
+      </ul>
     </div>  
   </div>
 } 
