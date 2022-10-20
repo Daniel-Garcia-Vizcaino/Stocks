d5b6c43a74a1aae9291c4389debd2093e2bdfd39
DanielGV <35636612+Daniel-Garcia-Vizcaino@users.noreply.github.com>
2022-10-20 15:22:04 +0000

    Set up addStock function

--- src/context/watchListContext.jsx
+++ src/context/watchListContext.jsx
@@ -5,8 +5,18 @@ export const WatchListContext = createContext()
 export const WatchListContextProvider = (props) => {
 
   const [watchList, setWatchList] = useState(["GOOGL", "MSFT", "AMZN"])
+
+  const addStock = (stock) => {
+    if (watchList.indexOf(stock) === -1) {
+      setWatchList([...watchList, stock])      
+    }
+
+  }
+
+  
   
   return <WatchListContext.Provider value={{ watchList }}>
     {props.children}
   </WatchListContext.Provider> 
 }
+
