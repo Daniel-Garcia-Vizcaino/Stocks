36ae3df1f771cc34100749db4e5c1d49abb78421
DanielGV <35636612+Daniel-Garcia-Vizcaino@users.noreply.github.com>
2022-10-20 14:16:04 +0000

    Set up search input

--- src/components/AutoComplete.jsx
+++ src/components/AutoComplete.jsx
@@ -1,3 +1,8 @@
 export const AutoComplete = () => {
-  return <div>AutoComplete</div>
+  return <div className="w-50 rounded mx-auto">
+    <div className="form-floating dropdown">
+      <input style={{backgroundColor: "rgba(146, 157, 172, 0.04)"}} id="search" type="text" className="form-control" placeholder="Search" autoComplete="off"></input>
+      <label htmlFor="search">Search</label>
+    </div>  
+  </div>
 } 
