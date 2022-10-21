afab4ee462acf23afe75588e0630760bf4202fa6
DanielGV <35636612+Daniel-Garcia-Vizcaino@users.noreply.github.com>
2022-10-21 14:07:13 +0000

    Add buttons to the StockChart component

--- src/components/StockChart.jsx
+++ src/components/StockChart.jsx
@@ -1,6 +1,8 @@
 import Chart from "react-apexcharts"
+import { useState } from "react"
 
 export const StockChart = ({chartData, symbol}) => {
+  const [dateFormat, setDateFormat] = useState("24h")
   const {day, week, year} = chartData
   const options = {
     title: {
@@ -35,5 +37,10 @@ export const StockChart = ({chartData, symbol}) => {
   
   return <div className="mt-5 p-4 shadow-sm bg-white">
     <Chart options={options} series={series} type="area" width="100%"/>
+    <div>
+      <button onClick={() => setDateFormat("24h")}>24h</button>
+      <button onClick={() => setDateFormat("7d")}>7d</button>
+      <button onClick={() => setDateFormat("1y")}>1y</button>
+    </div>
   </div>
 }
