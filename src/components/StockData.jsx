73eb5e167d098926bc036374aa735bbd29a66977
DanielGV <35636612+Daniel-Garcia-Vizcaino@users.noreply.github.com>
2022-10-21 15:36:56 +0000

    Make StockData component actually responsive

--- src/components/StockData.jsx
+++ src/components/StockData.jsx
@@ -29,34 +29,43 @@ export const StockData = ({symbol}) => {
         <div className="col">
           <div>
             <span className="fw-bold">Name: </span>
+            {stockData.name}
           </div>
           <div>
             <span className="fw-bold">Country: </span>
+            {stockData.country}
           </div>
           <div>
             <span className="fw-bold">Ticker: </span>
+            {stockData.ticker}
           </div>
         </div>
         <div className="col">
         <div>
             <span className="fw-bold">Exchange: </span>
+            {stockData.exchange}
           </div>
           <div>
             <span className="fw-bold">Industry: </span>
+            {stockData.finnhubIndustry}
           </div>
           <div>
             <span className="fw-bold">IPO: </span>
+            {stockData.ipo}
           </div>
         </div>
         <div className="col">
         <div>
             <span className="fw-bold">MarketCap: </span>
+            {stockData.marketCapitalization}
           </div>
           <div>
             <span className="fw-bold">Shares Outstanding: </span>
+            {stockData.shareOutstanding}
           </div>
           <div>
             <span className="fw-bold">Url: </span>
+            <a href={stockData.weburl}>{stockData.weburl}</a>
           </div>
         </div>
       </div>
