6d5bfafab78734ec59ff58671af99b7707e8e341
DanielGV <35636612+Daniel-Garcia-Vizcaino@users.noreply.github.com>
2022-10-21 16:22:59 +0000

    Add a header

--- src/pages/StockOverviewPage.jsx
+++ src/pages/StockOverviewPage.jsx
@@ -1,7 +1,8 @@
 import { AutoComplete } from '../components/AutoComplete'
 import { StockList } from '../components/StockList'
 export const StockOverviewPage = () => {
-  return <div>StockOverviewPage
+  return <div>
+    <h1 className="text-center mb-5">Choose your stocks</h1>
     <AutoComplete />
     <StockList />
   </div>
