e278ab2ff308f0d848f150ef085aed0d650fb492
DanielGV <35636612+Daniel-Garcia-Vizcaino@users.noreply.github.com>
2022-10-21 15:07:35 +0000

    Add remove button

--- src/components/StockList.jsx
+++ src/components/StockList.jsx
@@ -86,7 +86,7 @@ export const StockList = () => {
               <td>{stockData.data.h}</td>
               <td>{stockData.data.l}</td>
               <td>{stockData.data.o}</td>
-              <td>{stockData.data.pc}</td>
+              <td>{stockData.data.pc} <button className="btn btn-danger btn-sm ml-3 d-inline-block delete-button">Remove</button></td>
             </tr>    
           )
         })}
